6f75802ee12849278a6fdcba90231a06b87027b5
Steven Winter <doctor_speed_2001@yahoo.com>
2023-02-04 16:52:13 -0800

    Added comments, added virtual env. requirements

--- python/workstation/read_csv.py
+++ python/workstation/read_csv.py
@@ -1,19 +1,42 @@
-
-import pandas as pd
+""" Simple example of reading a CSV file and getting some statistics from it """
 import os
+import pandas as pd
 
-
+# GLOBALS
 CSV_FILE = 'python/workstation/data/booklist.csv'
+PRICE_COLUMN = 'price'
+PQT_FILE = 'python/workstation/data/booklist.pqt'
 
-print(os.getcwd())
-FileExists = os.path.exists(CSV_FILE)
-print('File ', CSV_FILE, 'Exists: ', FileExists)
+def check_for_file():
+    """_summary_ Print cwd and check for existence of CSV (global name) file
+    """
+    print(os.getcwd())
+    file_exists = os.path.exists(CSV_FILE)
+    print('File ', CSV_FILE, 'Exists: ', file_exists)
+
+def print_stats(interesting_dataframe):
+    """_summary_ Print some interesting things about this dataframe
+
+    Args:
+      interesting_dataframe (_type_): _description_ The dataframe to use for stats
+    """
+    mean_price = interesting_dataframe[PRICE_COLUMN].mean()
+    print('Mean price:', mean_price)
+    sum_prices = interesting_dataframe[PRICE_COLUMN].sum()
+    print('The sum of all the prices', sum_prices)
+    num_rows = interesting_dataframe.shape[0]
+    print('Number of rows:', num_rows)
+
+# main script 
+check_for_file()
 
 try:
     CsvFileDataFrame = pd.read_csv(CSV_FILE)
     print(CsvFileDataFrame.head())
-    print('read file')
-except Exception as err:
-  print('Error')
-  print(err)
-
+    print('done reading file\n------')
+    print_stats(CsvFileDataFrame)
+    CsvFileDataFrame.to_parquet(PQT_FILE)
+    print('done\n----')
+except IOError as io_error:
+    print(io_error)
+ 
